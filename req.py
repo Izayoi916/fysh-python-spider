d490caab9219b3557494cbe5700acfc0e6bb560e
Izayoi916 <145750852+Izayoi916@users.noreply.github.com>
2025-05-23 14:56:14 +0800

    req.py

--- req.py
+++ req.py
@@ -1,13 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
-url = "https://books.toscrape.com/"
 # url = "https://news.google.com/home?hl=zh-TW&gl=TW&ceid=TW:zh-Hant"
+url = "https://books.toscrape.com/"  # 替換為您要抓取的網站 URL
 response = requests.get(url)
-response.encoding = "utf-8"  # 因為該網頁編碼為 utf-8，加上 .encoding 避免亂碼
 
 if response.status_code == 200:
-    print("成功取得網頁內容")
-    print(response.text)
+    soup = BeautifulSoup(response.text, "html.parser")
+    title = soup.title.string
+    print(f"網頁標題：{title}")
+    print("所有連結：")
+    for link in soup.find_all("a"):
+        href = link.get("href")
+        if href:
+            print(href)
 else:
     print(f"無法取得網頁內容，狀態碼：{response.status_code}")
-    # print()
